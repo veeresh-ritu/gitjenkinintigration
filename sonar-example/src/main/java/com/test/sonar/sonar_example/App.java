a7179895863517ced56611cfa8da00fbc531ffb4
Your Name <veereshritu@gmail.com>
2020-10-19 18:08:07 +0530

    example

---
+++ sonar-example/src/main/java/com/test/sonar/sonar_example/App.java
@@ -0,0 +1,33 @@
+package com.test.sonar.sonar_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    class A{
+    	
+    	private String Test="";
+    	
+    	A()
+    	{
+    		
+    	}
+    	public void dummy(int nubmer)
+    	{
+    	 
+    		int number = 10;
+    		if(number==0)
+    		{
+    			System.out.println("Zero");
+    		}
+    		
+    	}
+    }
+}
